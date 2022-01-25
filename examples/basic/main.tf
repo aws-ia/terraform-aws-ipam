af6a4201b45cbde2b4db341bad2e5de63bec2482
drewmullen <drmullen@amazon.com>
2022-01-25 12:27:25 -0500

    output pools

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -16,5 +16,4 @@ module "basic" {
         }
     }
   }
-
 }
---
+++ examples/basic/outputs.tf
@@ -0,0 +1,11 @@
+output "top_level_pool" {
+    value = module.basic.top_level_pool
+}
+
+output "mid_level_pools" {
+    value = module.basic.mid_level_pools
+}
+
+output "region_level_pools" {
+    value = module.basic.region_level_pools
+}
