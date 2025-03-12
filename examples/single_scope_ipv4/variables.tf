c5eec272c7e0fa17656b93962ad022a6894f710f
Drew Mullen <drew.mullen@hashicorp.com>
2025-03-12 15:18:07 -0400

    set var defaults to null to help testing

--- examples/single_scope_ipv4/variables.tf
+++ examples/single_scope_ipv4/variables.tf
@@ -1,14 +1,17 @@
 variable "prod_account" {
   description = "Used for testing, prod account id"
   type        = list(string)
+  default     = null
 }
 
 variable "prod_ou_arn" {
   description = "arn of ou to share to prod accounts"
   type        = list(string)
+  default     = null
 }
 
 variable "sandbox_ou_arn" {
   description = "arn of ou to share to sandbox accounts"
   type        = list(string)
+  default     = null
 }
